eea432ae628313b87fb87cbf67c4c35eddc1d3c8
lunakenya <luna28kenya@gmail.com>
2025-01-27 13:07:23 -0500

    fix: :sparkles: Update the file orderRoutes.js

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -6,39 +6,37 @@ const orderController = require('../controllers/orderController');
  * @swagger
  * /orders/{id}:
  *   put:
- *     summary: Update an existing order
- *     description: Update the details of an existing order using its ID.
+ *     summary: Create a new order
+ *     description: Create a new logistics order with the provided details.
  *     tags:
  *       - Orders
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: integer
- *         description: The ID of the order to update
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
-            type: 'object',
-                    properties: {
-                        orderId: { type: 'integer', description: 'Unique identifier for the order' },
-                        senderName: { type: 'string', description: 'Name of the sender' },
-                        receiverName: { type: 'string', description: 'Name of the receiver' },
-                        packageDetails: { type: 'string', description: 'Details of the package' },
-                        shippingAddress: { type: 'string', description: 'Shipping address for the package' },
-                        status: { type: 'string', description: 'Current status of the order' },
-                    },
-                    required: ['senderName', 'receiverName', 'packageDetails','shippingAddress', 'status'],
-                },
+ *             type: object
+ *             properties:
+ *               "senderName":
+ *                 type: string
+ *                 example: Kenya Luna
+ *               "receiverName":
+ *                 type: string
+ *                 example: Alejandra Luna
+ *               "packageDetails":
+ *                 type: string
+ *                 example: Books and documents
+ *               "shippingAddress":
+ *                 type: string
+ *                 example: La Arcadia
  *     responses:
- *       200:
- *         description: Order updated successfully
- *       404:
- *         description: Order not found
+ *       '201':
+ *         description: Order created successfully
+ *       '500':
+ *         description: Internal server error
+ *       
  */
+
 
 // Route for updating an order
 router.put('/orders/:orderId', orderController.updateOrder);
