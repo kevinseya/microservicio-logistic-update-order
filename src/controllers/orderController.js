d39ee5c59b89a9e0ba269840ff03d7df544bafbc
lunakenya <luna28kenya@gmail.com>
2025-01-29 15:48:21 -0500

    fix: :bulb: update controller for UUID in the file orderController.js

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -3,11 +3,13 @@ const Order = require('../models/order');
 // Update an order by ID
 exports.updateOrder = async (req, res) => {
     try {
-        const { orderId } = req.params;
+        const { orderId } = req.params; // Tomar el UUID desde la URL
         const updatedOrder = await Order.update(req.body, { where: { orderId } });
+
         if (updatedOrder[0] === 0) {
             return res.status(404).json({ message: 'Order not found' });
         }
+
         res.status(200).json({ message: 'Order updated successfully' });
     } catch (error) {
         console.error('Error updating order:', error);
