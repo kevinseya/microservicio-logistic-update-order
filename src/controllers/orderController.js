5d28ada8285594ea6b334297ae10211a1f5cb8d5
lunakenya <luna28kenya@gmail.com>
2025-02-10 19:58:49 -0500

    feat: ✨ Update the project

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -4,19 +4,28 @@ const Order = require('../models/order');
 exports.updateOrderStatus = async (req, res) => {
     try {
         const { orderId } = req.params;
+        const { status } = req.body;  
 
-        // Buscar la orden por ID
+        
         const order = await Order.findOne({ where: { orderId } });
 
         if (!order) {
             return res.status(404).json({ message: 'Order not found' });
         }
 
-        // Cambiar el estado de activo a inactivo
-        order.active = !order.active;
+        
+        if (status) {
+            order.status = status;
+        }
+
         await order.save();
 
-        res.status(200).json({ message: 'Order status updated successfully', order });
+        
+        res.status(200).json({
+            message: 'Order status updated successfully',
+            order
+        });
+
     } catch (error) {
         console.error('Error updating order status:', error);
         res.status(500).json({ message: 'Failed to update order status' });
